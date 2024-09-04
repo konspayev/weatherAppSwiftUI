ab18b3bf1afd6b689f0a220947ab5f5418ad75cf
Nursultan Konspayev <Nursultan.Konspayev@gmail.com>
2024-09-04 19:34:09 +0500

    Initial commit

---
+++ WeatherAppSwiftUI/WeatherAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppSwiftUIApp.swift
+//  WeatherAppSwiftUI
+//
+//  Created by Nursultan Konspayev on 28.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
